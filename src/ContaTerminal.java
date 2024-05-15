7bbf7c133f1b48d860e536c77f0868b2ad3bde08
edavaldo wilson <develid.io@gmail.com>
2024-05-15 18:20:57 -0300

    Meu projeto conta banco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -10,6 +10,7 @@ public class ContaTerminal {
         scanner.nextLine(); // Limpar o buffer de entrada
         System.out.println("Por favor, digite seu nome");
         String nomeCliente = scanner.nextLine();
+        scanner.close();
         double saldo = 237.48;
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é: " + numeroAgencia +
                 ", conta: " + numeroConta + ", e seu saldo: " + saldo + ", já está disponível para saque.");
